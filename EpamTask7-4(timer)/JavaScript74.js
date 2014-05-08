d6e6e663a3454774179559d2fc753d2ed950bdcd
User <shiryaevaai92@gmail.com>
2014-05-08 10:54:10 +0300

    First

---
+++ EpamTask7-4(timer)/JavaScript74.js
@@ -0,0 +1,41 @@
+﻿var url;
+var pageTime;
+var timeOutID;
+var pr;
+//function PrintTime(t)
+//{
+//    return t;
+//}
+
+function NextPage(address) {
+    location.assign(address);
+}
+
+function Progress(addr, time) {
+    var a = "" + addr;
+    if (a != "undefined") {
+        url = "" + a;
+    }
+
+    var b = +time;
+    if (pageTime == undefined) {
+        pageTime = +b;
+    }
+
+    var bar = document.getElementById("LeftTime");
+    bar.innerHTML = pageTime;
+    pageTime--;
+
+    if (pageTime > 0) {
+        timeOutID = setTimeout(Progress, 1000);
+    }
+    else {
+        NextPage(url);
+    }
+}
+
+function StopProgress() {
+    clearTimeout(timeOutID);
+    pageTime = undefined;
+}
+
